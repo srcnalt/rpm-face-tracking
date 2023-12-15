eaf750c0a8ba89f770d15d7ddb0e9d763921958e
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-12-15 20:41:26 +0300

    setting up as in the doc

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ let handLandmarker: HandLandmarker;
 let enableWebcamButton: HTMLButtonElement;
 let webcamRunning: Boolean = false;
 
+
 let lastVideoTime = -1;
 let blendshapes: any[] = [];
 let rotation: Euler;
@@ -19,7 +20,7 @@ let headMesh: any[] = [];
 
 const options: HandLandmarkerOptions = {
   baseOptions: {
-    modelAssetPath: `https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task`,
+    modelAssetPath: `/models/hand_landmarker.task`,
     delegate: "GPU"
   },
   numHands: 2,
@@ -71,27 +72,63 @@ function App() {
     }
   });
 
-  const setup = async () => {
+  const createHandLandmarker = async () => {
     const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
     handLandmarker = await HandLandmarker.createFromOptions(vision, options);
+  }
 
+  // Check if webcam access is supported.
+  const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
+
+  // Enable the live webcam view and start detection.
+  const enableCam = () => {
+    if (!handLandmarker) {
+      console.log("Wait! objectDetector not loaded yet.");
+      return;
+    }
+
+    if (webcamRunning === true) {
+      webcamRunning = false;
+      enableWebcamButton.innerText = "ENABLE PREDICTIONS";
+    } else {
+      webcamRunning = true;
+      enableWebcamButton.innerText = "DISABLE PREDICTIONS";
+    }
+
+    // Get the video element.
     video = document.getElementById("video") as HTMLVideoElement;
-    navigator.mediaDevices.getUserMedia({
+
+    // getUsermedia parameters.
+    const constraints = {
       video: { width: 1280, height: 720 },
-      audio: false,
-    }).then(function (stream) {
+    };
+
+    // Activate the webcam stream.
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
       video.srcObject = stream;
       video.addEventListener("loadeddata", predict);
     });
+  }
+
+  const setup = async () => {
+    await createHandLandmarker();
+    // If webcam supported, add event listener to button for when user
+    // wants to activate it.
+    if (hasGetUserMedia()) {
+      enableWebcamButton = document.getElementById("webcamButton") as HTMLButtonElement;
+      enableWebcamButton.addEventListener("click", enableCam);
+    } else {
+      console.warn("getUserMedia() is not supported by your browser");
+    }
   }
 
   const predict = async () => {
     let nowInMs = Date.now();
     if (lastVideoTime !== video.currentTime) {
       lastVideoTime = video.currentTime;
-      const faceLandmarkerResult = handLandmarker.detectForVideo(video, nowInMs);
+      const handLandmarkerResult = handLandmarker.detectForVideo(video, nowInMs);
 
-      console.log(faceLandmarkerResult);
+      console.log(handLandmarkerResult);
 
 
       // if (faceLandmarkerResult.faceBlendshapes && faceLandmarkerResult.faceBlendshapes.length > 0 && faceLandmarkerResult.faceBlendshapes[0].categories) {
@@ -101,8 +138,8 @@ function App() {
       //   rotation = new Euler().setFromRotationMatrix(matrix);
       // }
     }
-
-    window.requestAnimationFrame(predict);
+    if (webcamRunning === true)
+      window.requestAnimationFrame(predict);
   }
 
   const handleOnChange = (event: any) => {
@@ -119,6 +156,10 @@ function App() {
         <p>Drag & drop RPM avatar GLB file here</p>
       </div>
       <input className='url' type="text" placeholder="Paste RPM avatar URL" onChange={handleOnChange} />
+      <button id="webcamButton" className="mdc-button mdc-button--raised">
+        <span className="mdc-button__ripple"></span>
+        <span className="mdc-button__label">ENABLE WEBCAM</span>
+      </button>
       <video className='camera-feed mirror-scene' id="video" autoPlay></video>
       {/* <Canvas className='mirror-scene' style={{ height: 600 }} camera={{ fov: 25 }} shadows>
         <ambientLight intensity={0.5} />
